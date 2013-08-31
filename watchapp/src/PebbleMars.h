7219c33f37a10d661b1d0f7c9fad1de68354961b
Tyler Gibbons <tyler.gibbons@ideogr.am>
2013-08-31 01:31:16 -0700

    Merged stuff and things

--- watchapp/src/PebbleMars.h
+++ watchapp/src/PebbleMars.h
@@ -8,8 +8,18 @@
 
 #define SHOW_METADATA FALSE
 
-void display_new_image(const GBitmap *image);
+void display_new_image();
 void set_footer_text(const char *text);
+
+/* Constants chosen for image size of 144px by 144px
+    Pebble requires num_bytes % 4 to equal zero
+  */
+uint8_t byte_buffer[144][20];
+
+void set_bitmap_byte(uint16_t index, uint8_t byte);
+void clear_bitmap();
+
+#define SHOW_METADATA FALSE
 #if SHOW_METADATA
 void set_metadata_text(const char* text);
 #endif
