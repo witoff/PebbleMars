ab0a530b8b885f6262cb770f8f4c935df11806d1
Tyler Gibbons <tyler.gibbons@ideogr.am>
2013-08-31 00:17:43 -0700

    Added update function prototypes to .h file

--- watchapp/src/PebbleMars.h
+++ watchapp/src/PebbleMars.h
@@ -6,3 +6,10 @@
 #define KEY_INSTRUMENT 3
 #define KEY_UTC 4
 
+#define SHOW_METADATA FALSE
+
+void display_new_image(const GBitmap *image);
+void set_footer_text(const char *text);
+#if SHOW_METADATA
+void set_metadata_text(const char* text);
+#endif
