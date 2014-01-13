32c575a74696f710efb4364f77f28d66d2093a09
Robert J Witoff <witoff@(none).(none)>
2014-01-13 02:59:24 +0000

    restricting number of images to 5 and replacing tabs with spaces

--- server/retriever.py
+++ server/retriever.py
@@ -29,57 +29,59 @@ print IMAGE_DIR_PROCESSED
 save_image_q = JoinableQueue()
 
 def getLatestUrl():
-	response = requests.get('http://mars.jpl.nasa.gov/msl-raw-images/image/image_manifest.json')
-	data = response.json()
+  response = requests.get('http://mars.jpl.nasa.gov/msl-raw-images/image/image_manifest.json')
+  data = response.json()
 
-	latest_sol = data['sols'][-1]
-	sol_url = latest_sol['catalog_url']
-	return sol_url
+  latest_sol = data['sols'][-1]
+  sol_url = latest_sol['catalog_url']
+  return sol_url
 
-	
+  
 def getLatestImages(image_count):
-	
-	response = requests.get(getLatestUrl())
-	data = response.json()
-	latest_utc = data['most_recent']
-	
-	images = data['images']
-	# Filter only the full images.
-	filtered = [i for i in images if i["sampleType"] == "full" and "CHEM" not in i["instrument"]]
-	
-	print 'filtered length: ', len(filtered)
-	if len(filtered) == 0:
-		print 'warning, filtered image len == 0.  Adding all images back in'
-		filtered = images 
-	#	print 'new filtered len: ', len(filtered)
+  
+  response = requests.get(getLatestUrl())
+  data = response.json()
+  latest_utc = data['most_recent']
+  
+  images = data['images']
+  # Filter only the full images.
+  filtered = [i for i in images if i["sampleType"] == "full" and "CHEM" not in i["instrument"]]
+  # No more than 5 images
+  filtered = filtered[:5]
+  
+  print 'filtered length: ', len(filtered)
+  if len(filtered) == 0:
+    print 'warning, filtered image len == 0.  Adding all images back in'
+    filtered = images 
+  #  print 'new filtered len: ', len(filtered)
 
-	metadata = []
-	for i in filtered:
-		metadata.append({'instrument' : i['instrument'],
-			'url' : i['urlList'],
-			'utc' : i['utc'],
-			'id' : i['itemName'],
-			'site' : i['site'],
-			'sol' : i['sol']
-			})
-	return latest_utc, metadata
+  metadata = []
+  for i in filtered:
+    metadata.append({'instrument' : i['instrument'],
+      'url' : i['urlList'],
+      'utc' : i['utc'],
+      'id' : i['itemName'],
+      'site' : i['site'],
+      'sol' : i['sol']
+      })
+  return latest_utc, metadata
 
 def saveRawImage(i):
-	# Downloads image and puts it in the queue.
-	res = None
-	try:
-		outFile = open(path.join(IMAGE_DIR_RAW, i['filename']), 'w')
-		request = requests.get(i['url'], stream=True)
-		for block in request.iter_content(1024):
-			if not block:
-				break
-			outFile.write(block)
-		outFile.close()
-		print "Saved %s" % i['id']
-		res = outFile.name
-	except Exception as e:
-		print "Error downloading image: %s, %s" % (i['url'], str(e))
-	return res
+  # Downloads image and puts it in the queue.
+  res = None
+  try:
+    outFile = open(path.join(IMAGE_DIR_RAW, i['filename']), 'w')
+    request = requests.get(i['url'], stream=True)
+    for block in request.iter_content(1024):
+      if not block:
+        break
+      outFile.write(block)
+    outFile.close()
+    print "Saved %s" % i['id']
+    res = outFile.name
+  except Exception as e:
+    print "Error downloading image: %s, %s" % (i['url'], str(e))
+  return res
 
 def chunks(l, n):
     """ Yield successive n-sized chunks from l.
@@ -88,134 +90,134 @@ def chunks(l, n):
         yield l[i:i+n]
 
 def saveRawImageWorker():
-	for item in iter(save_image_q.get, None):
-		saveRawImage(item)
-		save_image_q.task_done()
-	save_image_q.task_done()
+  for item in iter(save_image_q.get, None):
+    saveRawImage(item)
+    save_image_q.task_done()
+  save_image_q.task_done()
 
 def saveRawImages(images):
-	# Remove old images
-	print 'removing old images'
-	for name in os.listdir(IMAGE_DIR_RAW):
-		os.remove(path.join(IMAGE_DIR_RAW, name))
+  # Remove old images
+  print 'removing old images'
+  for name in os.listdir(IMAGE_DIR_RAW):
+    os.remove(path.join(IMAGE_DIR_RAW, name))
 
-	num_procs = 10
-	for group in chunks(images, num_procs):
-		procs = []
-		for item in group:
-			p = procs.append(Process(target=saveRawImageWorker))
-			procs[-1].daemon = True
-			procs[-1].start()
+  num_procs = 10
+  for group in chunks(images, num_procs):
+    procs = []
+    for item in group:
+      p = procs.append(Process(target=saveRawImageWorker))
+      procs[-1].daemon = True
+      procs[-1].start()
 
-		for i in group:
-			i['filename'] = i['id'] + '.jpg'
-			save_image_q.put(i)
+    for i in group:
+      i['filename'] = i['id'] + '.jpg'
+      save_image_q.put(i)
 
-		save_image_q.join()
+    save_image_q.join()
 
-		for p in procs:
-			save_image_q.put(None)
+    for p in procs:
+      save_image_q.put(None)
 
-		for p in procs:
-			p.join()
+    for p in procs:
+      p.join()
 
-	print "Saved %d images" % len(images)
+  print "Saved %d images" % len(images)
 
-	outManifest = open(path.join(IMAGE_DIR_RAW, 'manifest.json'), 'w')
-	outManifest.write(json.dumps(images))
-	outManifest.close()
-	#TODO: Save JSON manifest
+  outManifest = open(path.join(IMAGE_DIR_RAW, 'manifest.json'), 'w')
+  outManifest.write(json.dumps(images))
+  outManifest.close()
+  #TODO: Save JSON manifest
 
 def getImageData(filename):
-	#Load
-	img = Image.open(filename) # open colour image
-	
-	#Scale
-	dims = (IMAGE_WIDTH, IMAGE_HEIGHT)
-	print 'resizing from ', img.size, ' to dims ', dims
-	img = img.resize(dims, Image.ANTIALIAS)
-	print 'new size: ', img.size
-	
-	#Black and white
-	img = img.convert('1') # convert image to black and white
+  #Load
+  img = Image.open(filename) # open colour image
+  
+  #Scale
+  dims = (IMAGE_WIDTH, IMAGE_HEIGHT)
+  print 'resizing from ', img.size, ' to dims ', dims
+  img = img.resize(dims, Image.ANTIALIAS)
+  print 'new size: ', img.size
+  
+  #Black and white
+  img = img.convert('1') # convert image to black and white
 
-	#Save Temp
-	img.save(path.join(IMAGE_DIR_PROCESSED, filename.split('/')[-1].split('.')[0] + ".png"))
+  #Save Temp
+  img.save(path.join(IMAGE_DIR_PROCESSED, filename.split('/')[-1].split('.')[0] + ".png"))
 
-	# Convert to bitstream
-	data_bits = []
-	for i in range(img.size[1]):
-		for j in range(img.size[0]):
-			data_bits.append(int(bool(img.getpixel((j,i)))))
-		if IMAGE_PADDING_BYTES > 0:
-			for k in range(IMAGE_PADDING_BYTES * 8):
-				data_bits.append(0)
-	return data_bits
+  # Convert to bitstream
+  data_bits = []
+  for i in range(img.size[1]):
+    for j in range(img.size[0]):
+      data_bits.append(int(bool(img.getpixel((j,i)))))
+    if IMAGE_PADDING_BYTES > 0:
+      for k in range(IMAGE_PADDING_BYTES * 8):
+        data_bits.append(0)
+  return data_bits
 
 def processImages(utc):
-	image_files = os.listdir(IMAGE_DIR_RAW)
-	f = open(path.join(IMAGE_DIR_RAW, 'manifest.json'), 'r')
-	manifest = json.loads(f.read())
-	f.close()
+  image_files = os.listdir(IMAGE_DIR_RAW)
+  f = open(path.join(IMAGE_DIR_RAW, 'manifest.json'), 'r')
+  manifest = json.loads(f.read())
+  f.close()
 
-	response = []
-	for obj in manifest:
-		data_bits = getImageData(path.join(IMAGE_DIR_RAW, obj['filename']))
-		data_str = [str(d) for d in data_bits]
-		data_bytes = []
-		pos = 0
-		for chunk_id in range(IMAGE_CHUNKS):
-			chunk_bytes = [struct.pack('B', chunk_id)]
-			for j in range(chunk_id, chunk_id+IMAGE_ROWS_PER_CHUNK):
-				for i in range(int(math.ceil(IMAGE_COLS))):
-					nums = data_str[pos:pos+WORD_BITS][::-1]
-					pos += WORD_BITS
-					if len(nums) == 0:
-						break
-					chunk_bytes.append(struct.pack('I', int(''.join(nums), 2)))
-			if len(chunk_bytes) <= 1:
-				break
-			data_bytes.append(base64.b64encode(''.join(chunk_bytes)))
+  response = []
+  for obj in manifest:
+    data_bits = getImageData(path.join(IMAGE_DIR_RAW, obj['filename']))
+    data_str = [str(d) for d in data_bits]
+    data_bytes = []
+    pos = 0
+    for chunk_id in range(IMAGE_CHUNKS):
+      chunk_bytes = [struct.pack('B', chunk_id)]
+      for j in range(chunk_id, chunk_id+IMAGE_ROWS_PER_CHUNK):
+        for i in range(int(math.ceil(IMAGE_COLS))):
+          nums = data_str[pos:pos+WORD_BITS][::-1]
+          pos += WORD_BITS
+          if len(nums) == 0:
+            break
+          chunk_bytes.append(struct.pack('I', int(''.join(nums), 2)))
+      if len(chunk_bytes) <= 1:
+        break
+      data_bytes.append(base64.b64encode(''.join(chunk_bytes)))
 
-		utc = utc.split(".")[0].split("Z")[0]
-		secs = time.mktime(time.gmtime()) - time.mktime(time.strptime(utc, "%Y-%m-%dT%H:%M:%S"))
-		hours = int(secs/3600)
-		if hours == 0:
-			rel_time = str(int(secs/61)) + ' mins '
-		else:
-			rel_time = str(hours) + ' hours '
-		rel_time += 'ago'
+    utc = utc.split(".")[0].split("Z")[0]
+    secs = time.mktime(time.gmtime()) - time.mktime(time.strptime(utc, "%Y-%m-%dT%H:%M:%S"))
+    hours = int(secs/3600)
+    if hours == 0:
+      rel_time = str(int(secs/61)) + ' mins '
+    else:
+      rel_time = str(hours) + ' hours '
+    rel_time += 'ago'
 
-		response.append({
-			#'data' : data,
-			'data_bytes' : data_bytes,
-			'width' : IMAGE_WIDTH,
-			'height' : IMAGE_HEIGHT,
-			'rel_time' : rel_time,
-			'filename' : obj['filename'].replace('jpg', 'png'),
-			'instrument' : obj['instrument'],
-			'utc' : obj['utc'],
-			'site': obj['site'],
-			'sol': obj['sol']
-		})
-	print 'Writing output file...'
-	f = open(path.join(IMAGE_DIR_PROCESSED, 'manifest.json'), 'w')
-	f.write(json.dumps(response))
-	f.close()
-	return response
+    response.append({
+      #'data' : data,
+      'data_bytes' : data_bytes,
+      'width' : IMAGE_WIDTH,
+      'height' : IMAGE_HEIGHT,
+      'rel_time' : rel_time,
+      'filename' : obj['filename'].replace('jpg', 'png'),
+      'instrument' : obj['instrument'],
+      'utc' : obj['utc'],
+      'site': obj['site'],
+      'sol': obj['sol']
+    })
+  print 'Writing output file...'
+  f = open(path.join(IMAGE_DIR_PROCESSED, 'manifest.json'), 'w')
+  f.write(json.dumps(response))
+  f.close()
+  return response
 
 def main(image_count):
-	latest_utc, images = getLatestImages(image_count)
-	pprint(images)
-	saveRawImages(images)
-	data = processImages(latest_utc)
-	
-	
+  latest_utc, images = getLatestImages(image_count)
+  pprint(images)
+  saveRawImages(images)
+  data = processImages(latest_utc)
+  
+  
 if __name__ == '__main__':
-	main(5)
-	
-	
-	
-	
+  main(5)
+  
+  
+  
+  
 
 
--- server/uploader.py
+++ server/uploader.py
@@ -15,58 +15,58 @@ RDF_PUBLIC_URL = "https://%s.s3.amazonaws.com/update_rdf.json" % (BUCKET_NAME)
 MANIFEST_PUBLIC_URL = "https://%s.s3.amazonaws.com/manifest.json" % (BUCKET_NAME)
 
 def main():
-	conn = boto.connect_s3()
-	bucket = conn.lookup(BUCKET_NAME)
+  conn = boto.connect_s3()
+  bucket = conn.lookup(BUCKET_NAME)
 
-	manifest_path = path.join(FILE_ROOT,"manifest.json")
-	if not os.path.exists(manifest_path):
-		print "ERROR: Manifest file not found to process"
-		return
+  manifest_path = path.join(FILE_ROOT,"manifest.json")
+  if not os.path.exists(manifest_path):
+    print "ERROR: Manifest file not found to process"
+    return
 
-	manifest = json.loads(open(manifest_path,'r').read())
+  manifest = json.loads(open(manifest_path,'r').read())
 
-	# Upload the manifest
-	fname = manifest_path
-	print 'Uploading %s to Amazon S3 bucket %s' % \
-				 (fname, BUCKET_NAME)
-	def percent_cb(complete, total):
-			sys.stdout.write('.')
-			sys.stdout.flush()
+  # Upload the manifest
+  fname = manifest_path
+  print 'Uploading %s to Amazon S3 bucket %s' % \
+         (fname, BUCKET_NAME)
+  def percent_cb(complete, total):
+      sys.stdout.write('.')
+      sys.stdout.flush()
 
-	k = Key(bucket)
-	k.key = path.basename(fname)
-	k.set_contents_from_filename(fname,
-		cb=percent_cb, num_cb=10)
+  k = Key(bucket)
+  k.key = path.basename(fname)
+  k.set_contents_from_filename(fname,
+    cb=percent_cb, num_cb=10)
 
-	k.make_public()
-	print ""
+  k.make_public()
+  print ""
 
-	# Upload the RDF
-	new_rdf = {
-		"manifest_version": manifest[0]['sol'],
-		"url": MANIFEST_PUBLIC_URL,
-		"ttl": 3600
-	}
+  # Upload the RDF
+  new_rdf = {
+    "manifest_version": manifest[0]['sol'],
+    "url": MANIFEST_PUBLIC_URL,
+    "ttl": 3600
+  }
 
-	fname = path.join(FILE_ROOT,"update_rdf.json")
+  fname = path.join(FILE_ROOT,"update_rdf.json")
 
-	f = open(fname,'w')
-	f.write(json.dumps(new_rdf))
-	f.close()
+  f = open(fname,'w')
+  f.write(json.dumps(new_rdf))
+  f.close()
 
-	print 'Uploading %s to Amazon S3 bucket %s' % \
-				 (fname, BUCKET_NAME)
-	def percent_cb(complete, total):
-			sys.stdout.write('.')
-			sys.stdout.flush()
+  print 'Uploading %s to Amazon S3 bucket %s' % \
+         (fname, BUCKET_NAME)
+  def percent_cb(complete, total):
+      sys.stdout.write('.')
+      sys.stdout.flush()
 
-	k = Key(bucket)
-	k.key = path.basename(fname) 
-	k.set_contents_from_filename(fname,
-		cb=percent_cb, num_cb=10)
+  k = Key(bucket)
+  k.key = path.basename(fname) 
+  k.set_contents_from_filename(fname,
+    cb=percent_cb, num_cb=10)
 
-	k.make_public()
-	print ""
+  k.make_public()
+  print ""
 
 if __name__ == '__main__':
-	main()
+  main()
